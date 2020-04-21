5457bb11ec0a93d7d175e50f3358ced13230b3b9
José Anota <xpapox1070@icloud.com>
2020-04-21 01:25:04 -0500

    Se integro la bar y el footer oficial del SPR

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '.:SPR:. - POSTMAN -') }}</title>
+    <title>SPR | POSTMAN </title>
+    <link rel="shortcut icon" href="\images\icon.ico">
+
 
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
@@ -17,64 +20,75 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('css/colors.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body style="background-color:#ffffff">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+        <nav class="navbar navbar-expand-sm colorspr">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', '.:SPR:. - POSTMAN -') }}
+                    <img src="{{ asset('images/logo_spr.png') }}" width="110" height="60" alt=""> <span class="text-white">Sistema Publico de Radiodifusión del Estado Mexicano</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                <div class="collapse navbar-collapse" id="navbarstyle">
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav pull-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+
+                        <button type="button" class="btn btn-light btn-sm nav-item">
+                            <a class="nav-link " href="{{ route('login') }}"><span class="text-dark">{{ __('Entrar') }}</span></a>
+                        </button>
+                        <button type="button" class="btn btn-light btn-sm nav-item" style="margin-left:20px">
+                            <a class="nav-link " href="{{ route('register') }}"><span class="text-dark">{{ __('Registrar') }}</span></a>
+                        </button>                        
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        @if( Auth::user()->email_verified_at != null )
+                        @endif
+                        <li class="nav-item dropdown">
+
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} {{ Auth::user()->ap_paterno }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
-
-        <main class="py-4">
+        <main class>
             @yield('content')
         </main>
     </div>
-</body>
+<footer class="container-fluid footer">
+        <div class="row pre-footer">
+            <div class="col-sm">
+                <p class="ml-5 mt-3 ">2020 | <strong> Sistema Publico de Radiodifusión del Estado Méxicano </strong>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row colorspr">
+        <div class="col-sm">
+            <p class="ml-5 mt-3 text-white">spr.gob.mx
+            </p>
+        </div>
+    </div>
+</footer>
 </html>
