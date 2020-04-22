88d4b4e33bcb3e334bab94d26f2445a69b19adae
José Anota <xpapox1070@icloud.com>
2020-04-21 23:32:53 -0500

    Se agregaron datos al controlador para la validacion, terminar modificacion.

--- app/Http/Controllers/CorrespondenciaEnvController.php
+++ app/Http/Controllers/CorrespondenciaEnvController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class CorrespondenciaEnvController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'folioR' => 'required|max:255',
+            'fechaRR' => 'required',
+            'horaRR' => 'required|numeric',
+            'unidadSR' => 'required|max:255',
+            'numeroOR' => 'required',
+            'fechaOR' => 'required|numeric',
+            'horaOR' => 'required|max:255',
+            'contenidoPR' => 'required',
+            'justificacionPR' => 'required|numeric',
+            'urgentePR' => 'required',
+            'terminosPR' => 'required|numeric'
+        ]);
+
+    }//
 }
