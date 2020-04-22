f0e4fa7bc72c858e3ed65e3f4d5ab4bbb3091dc4
José Anota <xpapox1070@icloud.com>
2020-04-21 21:46:33 -0500

    Se agregaron los detalles a la ventana de envío de documento.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="jumbotron jumbotron-fluid mt-5">
-    <div class="container">
-      <h1 class="display-4">Sistema Publico de Radiodifusión del Estado Méxicano | PostMan |</h1>
-      <p class="lead">Servicio de rececpción y envío.</p>
-    </div>
+  <div class="container">
+    <h1 class="display-4">Sistema Publico de Radiodifusión del Estado Méxicano | PostMan |</h1>
+    <p class="lead">Servicio de rececpción y envío.</p>
   </div>
+</div>
 @endsection
